881eaba462141fcde7b3622206797e0fb9c85b43
NISHANTH KN <nishanthnishanth47622@gmail.com>
2026-02-22 07:32:01 +0530

    Add Sum.py for user input and summation

---
+++ Sum.py
@@ -0,0 +1,7 @@
+#Getting user input and doing math
+name = input("Enter your name: ")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+sum = num1 + num2
+print(f"Hi {name}, the sum of {num1} and {num2} is: {sum}")
+
